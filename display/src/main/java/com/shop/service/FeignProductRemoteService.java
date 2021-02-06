a07a571f873100f8f2ae9ddf8ebca3ac2b693c75
apio1130 <apio1130@naver.com>
2021-02-06 21:23:42 +0900

    feat(step-5): Feign과 Ribbon, Eureka 연동
    - Feign의 URL 정보를 Ribbon과 Eureka를 이용하여 전달 받음(URL 제거)

--- display/src/main/java/com/shop/service/FeignProductRemoteService.java
+++ display/src/main/java/com/shop/service/FeignProductRemoteService.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="product", url = "http://localhost:8086/")
+@FeignClient(name="product")
 public interface FeignProductRemoteService {
     @GetMapping("/products/{productId}")
     String getProductInfo(@PathVariable("productId") String productId);
