f5a3e4ea89530669cc09443d25e5758fc9daba5b
apio1130 <apio1130@naver.com>
2021-02-06 21:42:31 +0900

    feat(step-5): Feign과 Hysteix, Ribbon, Eureka 조합
    - Hystrix의 Fallback 처리 관련 Component 추가
    - Feign용 Hystrix fallback 프로퍼티 추가

--- display/src/main/java/com/shop/service/FeignProductRemoteService.java
+++ display/src/main/java/com/shop/service/FeignProductRemoteService.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="product")
+@FeignClient(name="product", fallback = FeignProductRemoteServiceFallbackImpl.class)
 public interface FeignProductRemoteService {
     @GetMapping("/products/{productId}")
     String getProductInfo(@PathVariable("productId") String productId);
---
+++ display/src/main/java/com/shop/service/FeignProductRemoteServiceFallbackImpl.java
@@ -0,0 +1,12 @@
+package com.shop.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FeignProductRemoteServiceFallbackImpl implements FeignProductRemoteService {
+
+    @Override
+    public String getProductInfo(String productId) {
+        return "[this product is sold out]";
+    }
+}
